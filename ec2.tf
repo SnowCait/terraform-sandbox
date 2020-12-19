2aed2424a13e895df72640a17d06a68c4a8ee939
雪猫 <SnowCait@users.noreply.github.com>
2020-12-20 04:45:29 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,13 +17,13 @@ provider "aws" {
 # }
 
 resource "aws_instance" "example" {
-  count         = 0
+#   count         = 0
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "${format("example-%02d", count.index + 1)}"
-  }
+#   tags = {
+#     Name = "${format("example-%02d", count.index + 1)}"
+#   }
 }
 
 # resource "aws_instance" "sandbox" {
