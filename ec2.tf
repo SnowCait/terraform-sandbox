056f48a11ff8691fc470656fdb54b71470ee44c2
雪猫 <SnowCait@users.noreply.github.com>
2020-12-20 04:46:56 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,27 +11,8 @@ provider "aws" {
   profile = "default"
   region  = "ap-northeast-1"
 }
-# provider "aws" {
-#   version = ">= 2.0"
-#   region  = "ap-northeast-1"
-# }
 
 resource "aws_instance" "example" {
-#   count         = 0
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "${format("example-%02d", count.index + 1)}"
-#   }
 }
-
-# resource "aws_instance" "sandbox" {
-#   count         = 0
-#   ami           = "ami-02b658ac34935766f"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "${format("sandbox-%02d", count.index + 1)}"
-#   }
-# }
