e5bf3e59a8415d8a50754d43d53760938ba73065
雪猫 <SnowCait@users.noreply.github.com>
2020-12-20 04:41:58 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,33 @@
-resource "aws_instance" "sandbox" {
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example" {
   count         = 0
-  ami           = "ami-02b658ac34935766f"
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "${format("sandbox-%02d", count.index + 1)}"
+    Name = "${format("example-%02d", count.index + 1)}"
   }
 }
+
+# resource "aws_instance" "sandbox" {
+#   count         = 0
+#   ami           = "ami-02b658ac34935766f"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "${format("sandbox-%02d", count.index + 1)}"
+#   }
+# }
