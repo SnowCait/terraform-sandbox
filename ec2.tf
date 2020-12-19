0c3f3e9cb3d25491d496179591148691b737cb48
雪猫 <SnowCait@users.noreply.github.com>
2020-12-20 06:44:13 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,8 +10,14 @@ terraform {
 provider "aws" {}
 
 resource "aws_instance" "example" {
-  count = 0
+  count = 1
 
   ami           = "ami-02b658ac34935766f"
   instance_type = "t2.micro"
+
+  user_data = "curl -s https://raw.githubusercontent.com/actions/runner/automate/scripts/create-latest-svc.sh | bash -s SnowCait/terraform-sandbox"
+
+  tags = {
+    Name = "GitHub Actions ${count.index}"
+  }
 }
