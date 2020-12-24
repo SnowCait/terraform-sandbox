829488efc32330b77f2c383724a2893ba7796e01
SnowCait <SnowCait@users.noreply.github.com>
2020-12-25 02:30:36 +0900

    Revert "Delete main.tf"
    This reverts commit 422430b5a69c63513123b248a94f6ce63fe8a820.

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+resource "aws_instance" "github-actions" {
+  count = 1
+
+  ami           = "ami-02b658ac34935766f"
+  instance_type = "t2.micro"
+
+  user_data = "curl -s https://raw.githubusercontent.com/actions/runner/automate/scripts/create-latest-svc.sh | bash -s SnowCait/terraform-sandbox"
+
+  tags = {
+    Name = "GitHub Actions ${count.index}"
+  }
+}
