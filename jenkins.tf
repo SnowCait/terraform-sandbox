7e0e5d847c5176107d0058b58d67102d02ded1b5
雪猫 <SnowCait@users.noreply.github.com>
2021-01-23 04:28:08 +0900

    Create jenkins.tf

---
+++ jenkins.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+resource "aws_instance" "jenkins" {
+  count = 0
+
+  ami = "ami-0b313cea00b6ad211"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins ${count.index}"
+  }
+}
